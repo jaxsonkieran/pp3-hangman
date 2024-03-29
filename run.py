f54833b5bad39e441c6b69229b0fc146012d1fb5
Kieran Jackson <jaxsonkieran@gmail.com>
2024-03-29 18:27:10 +0000

    add user input and user guess to hangman function

--- run.py
+++ run.py
@@ -18,9 +18,19 @@ def hangman():
     word = choose_word(words)
     word_letters = set(word) # keeping track of whats already been guessed in the word
     alphabet = set(string.ascii_uppercase)
-    used_letters = set() # what the user guesses
-
+    letters_guessed = set() # what the user guesses
+    # getting user input
     user_guess = input("Guess a letter: ").upper()
+    if user_guess in alphabet - letters_guessed:
+        letters_guessed.add(user_guess)
+        if user_guess in word_letters:
+            word_letters.remove(user_guess)
+
+    elif user_guess in letters_guessed:
+        print("You've already guessed this letter. Please try again.")
+    
+    else:
+        print("Error. Please type in a valid letter.")
 
 user_input = input("Please type in a letter: ")
 print(user_input)

4321a8edc5d08c13cdd24e4dde07223d5273ade6
Kieran Jackson <jaxsonkieran@gmail.com>
2024-03-31 13:34:41 +0000

    add: function for user welcome, which introduces and describes rules for user, import time/sleep

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 
 import random
 from words import words
+from time import sleep
 
 # Function to choose a random word from the words.py list
 # This function was really helped by the json file from Kylie Ying in her YT tutorial
@@ -107,15 +108,17 @@ def display_word(word, guessed_letters, incorrect_guesses):
 
 # Function to play the hangman game
 def hangman():
+
+    user_welcome()
     # Ask the user to enter their name
     name = input("Enter your name: ")
-    print(f"Welcome {name}! Let's get ready to play hangman :)")
+    print(f"Welcome {name}! Let's get ready to play!\n")
 
     # Difficulty selection
-    print("Choose your difficulty!")
-    print("Enter e for Easy, this will give you 8 lives")
-    print("Enter m for Medium, this will give you 6 lives")
-    print("Enter h for Hard, this will give you 4 lives")
+    print("Choose your difficulty!\n")
+    print("- Enter e for Easy, this will give you 8 lives")
+    print("- Enter m for Medium, this will give you 6 lives")
+    print("- Enter h for Hard, this will give you 4 lives")
 
     difficulty = input("Enter your choice (e/m/h): ").lower()
     if difficulty == "e":
@@ -182,6 +185,23 @@ def hangman():
         hangman()
     else:
         print("Thanks for playing hangman, hope you enjoyed :)")   
+
+
+def user_welcome():
+    """
+    WElcome user to the game
+    give rules of the game
+    choose a level
+    """
+    print("Welcome to Hangman! \n")
+    sleep(1)
+    print("Try to guess the random word before you get hung\n")
+    sleep(1)
+    print("Follow the instructions below to choose a level: Easy, Medium, Hard.\n")
+    sleep(1)
+    print("----------------------------------------")
+    sleep(1)
+
 
 # Start the game
 hangman()

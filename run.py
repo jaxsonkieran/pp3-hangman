8f1e7ac5e3bb918199c88fb55b21f8ee99e6d7f8
Kieran Jackson <jaxsonkieran@gmail.com>
2024-03-31 12:32:08 +0000

    Implement function to display word with blanks and guessed letters

--- run.py
+++ run.py
@@ -90,6 +90,16 @@ def display_hangman(lives):
 # Function to check if the guessed letter is in the word
 
 # Function to display the word with blanks and guessed letters
+def display_word(word, guessed_letters, incorrect_guesses):
+    displayed_word = ""
+    for letter in word:
+        if letter in guessed_letters:
+            displayed_word += letter
+        else:
+            displayed_word += "_ "
+    
+    displayed_word += "\nIncorrect guesses: " + ", ".join(incorrect_guesses)
+    return displayed_word
 
 # Function to check if the guessed word matches the chosen word
 
@@ -101,19 +111,20 @@ def hangman():
     word_letters = set(word) # keeping track of whats already been guessed in the word
     alphabet = set(string.ascii_uppercase)
     letters_guessed = set() # what the user guesses
-    # getting user input
-    user_guess = input("Guess a letter: ").upper()
-    if user_guess in alphabet - letters_guessed:
-        letters_guessed.add(user_guess)
-        if user_guess in word_letters:
-            word_letters.remove(user_guess)
-
-    elif user_guess in letters_guessed:
-        print("You've already guessed this letter. Please try again.")
     
-    else:
-        print("Error. Please type in a valid letter.")
+#     # getting user input
+#     user_guess = input("Guess a letter: ").upper()
+#     if user_guess in alphabet - letters_guessed:
+#         letters_guessed.add(user_guess)
+#         if user_guess in word_letters:
+#             word_letters.remove(user_guess)
 
-user_input = input("Please type in a letter: ")
-print(user_input)
+#     elif user_guess in letters_guessed:
+#         print("You've already guessed this letter. Please try again.")
+    
+#     else:
+#         print("Error. Please type in a valid letter.")
+
+# user_input = input("Please type in a letter: ")
+# print(user_input)
     
